68e6f29dc8d54c56f059a2ee3ea4648d2d26ee3e
wayneliang(梁宇浩) <wayneliang@futunn.com>
2022-09-16 17:38:37 +0800

    save

--- sub-app/react-ts-app/src/pages/404.tsx
+++ sub-app/react-ts-app/src/pages/404.tsx
@@ -1,4 +1,19 @@
+import { useShared } from '@/context/SharedContext';
+import { useEffect } from 'react';
+
 function Page404() {
+  const shared = useShared();
+
+  useEffect(() => {
+    // @ts-ignore
+    if (window.__POWERED_BY_QIANKUN__) {
+      shared!.dispatch({
+        type: 'CHANGE_ROUTE',
+        payload: '/404',
+      });
+    }
+  }, [shared]);
+
   return <div style={{ marginTop: '20vh', fontSize: '18px', textAlign: 'center' }}>404</div>;
 }
 
