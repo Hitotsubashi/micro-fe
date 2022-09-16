b65d375c04eaf3ea5557fe63c97fd197756e53ac
wayneliang(梁宇浩) <wayneliang@futunn.com>
2022-09-16 20:17:40 +0800

    save

--- master-app/src/router/modules/micro-app.js
+++ master-app/src/router/modules/micro-app.js
@@ -1,85 +1,85 @@
-import Layout from "@/layout";
-import MicroAppLayout from "@/layout/MicroAppLayout";
-import Vue from "vue";
-import { loader, shared } from "@/qiankun";
+import Layout from '@/layout'
+import MicroAppLayout from '@/layout/MicroAppLayout'
+import Vue from 'vue'
+import { loader, shared } from '@/qiankun'
 
 const microAppRoutes = [
   {
-    path: "/app-react",
+    path: '/app-react',
     component: Layout,
     children: [
       {
-        path: "index*",
-        name: "app-react",
-        component: Vue.extend({ ...MicroAppLayout, name: "AppReact" }),
-        props: { id: "app-react" },
+        path: 'index*',
+        name: 'AppReact',
+        component: Vue.extend({ ...MicroAppLayout, name: 'AppReact' }),
+        props: { id: 'app-react' },
         meta: {
-          title: "ReactTSApp",
+          title: 'ReactTSApp',
           microApp: {
-            name: "react app", // app name registered
-            entry: "//localhost:3001",
-            container: "#app-react",
+            name: 'react app', // app name registered
+            entry: '//localhost:3001',
+            container: '#app-react',
             loader,
-            activeRule: "/app-react/index",
-            props: { shared },
+            activeRule: '/app-react/index',
+            props: { shared }
           },
           noCache: true,
-          menuPath: "index",
-          icon: "el-icon-coin",
-        },
-      },
-    ],
+          menuPath: 'index',
+          icon: 'el-icon-coin'
+        }
+      }
+    ]
   },
   {
-    path: "/app-vue",
+    path: '/app-vue',
     component: Layout,
     children: [
       {
-        path: "index*",
-        name: "app-vue",
-        component: Vue.extend({ ...MicroAppLayout, name: "AppVue" }),
-        props: { id: "app-vue" },
+        path: 'index*',
+        name: 'AppVue',
+        component: Vue.extend({ ...MicroAppLayout, name: 'AppVue' }),
+        props: { id: 'app-vue' },
         meta: {
-          title: "VueApp",
+          title: 'VueApp',
           microApp: {
-            name: "vue app", // app name registered
-            entry: "//localhost:3002",
-            container: "#app-vue",
+            name: 'vue app', // app name registered
+            entry: '//localhost:3002',
+            container: '#app-vue',
             loader,
-            activeRule: "/app-vue/index",
-            props: { shared },
+            activeRule: '/app-vue/index',
+            props: { shared }
           },
           noCache: true,
-          menuPath: "index",
-          icon: "el-icon-coin",
-        },
-      },
-    ],
+          menuPath: 'index',
+          icon: 'el-icon-coin'
+        }
+      }
+    ]
   },
   {
-    path: "/app-purehtml",
+    path: '/app-purehtml',
     component: Layout,
     children: [
       {
-        path: "index",
-        name: "app-purehtml",
-        component: Vue.extend({ ...MicroAppLayout, name: "AppPurehtml" }),
-        props: { id: "app-purehtml" },
+        path: 'index',
+        name: 'AppPurehtml',
+        component: Vue.extend({ ...MicroAppLayout, name: 'AppPurehtml' }),
+        props: { id: 'app-purehtml' },
         meta: {
-          title: "PureHTMLApp",
+          title: 'PureHTMLApp',
           microApp: {
-            name: "purehtml app", // app name registered
-            entry: "//localhost:3003",
-            container: "#app-purehtml",
+            name: 'purehtml app', // app name registered
+            entry: '//localhost:3003',
+            container: '#app-purehtml',
             loader,
-            activeRule: "/app-purehtml/index",
-            props: { shared },
+            activeRule: '/app-purehtml/index',
+            props: { shared }
           },
-          icon: "el-icon-coin",
-        },
-      },
-    ],
-  },
-];
+          icon: 'el-icon-coin'
+        }
+      }
+    ]
+  }
+]
 
-export default microAppRoutes;
+export default microAppRoutes
