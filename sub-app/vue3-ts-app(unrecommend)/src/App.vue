2ced1b69684e655fc1a372e786cc7b583ca8680c
wayneliang(梁宇浩) <wayneliang@futunn.com>
2022-09-28 16:58:39 +0800

    备份带路由的vue3项目

---
+++ sub-app/vue3-ts-app(unrecommend)/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useMicroApp } from "@/hooks/useMicroApp";
+
+export default defineComponent({
+  name: "Vue3TSApp",
+  setup() {
+    if (window.__POWERED_BY_QIANKUN__) {
+      useMicroApp();
+    }
+  },
+});
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ sub-app/vue3-ts-app(unrecommend)/src/main.ts
@@ -0,0 +1,40 @@
+import "./public-path";
+import { createApp } from "vue";
+import App from "./App.vue";
+import { getRouter, destroyRouter } from "./router";
+import pinia from "./pinia";
+import { useAppStore } from "./pinia/modules/app";
+
+let instance: ReturnType<typeof createApp> | null = null;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function render(props: any) {
+  const { container, shared,basepath } = props;
+  instance = createApp(App).use(pinia).use(getRouter(basepath))
+  if (window.__POWERED_BY_QIANKUN__) {
+    instance.provide("$shared", shared);
+  }
+  instance.mount(container ? container.querySelector("#app") : "#app");
+}
+
+if (!window.__POWERED_BY_QIANKUN__) {
+  render({});
+}
+
+export async function bootstrap() {
+  console.log("%c%s", "color: green;", "vue3.0 app bootstraped");
+}
+
+export async function mount(props: any) {
+  render(props);
+  props.onGlobalStateChange((state: any) => {
+    const app = useAppStore();
+    app.changeTheme(state.theme);
+  });
+}
+
+export async function unmount() {
+  instance!.unmount();
+  instance!._container!.innerHTML = "";
+  destroyRouter();
+}
