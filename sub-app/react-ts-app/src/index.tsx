b2cf9c3428c3808d0c2950368e7733e088842a77
wayneliang(梁宇浩) <wayneliang@futunn.com>
2022-11-11 10:53:22 +0800

    test time: edit develop code

--- sub-app/react-ts-app/src/index.tsx
+++ sub-app/react-ts-app/src/index.tsx
@@ -1,11 +1,11 @@
-import './public-path';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import './public-path';
+import reportWebVitals from './reportWebVitals';
 import store from './store';
 import { appActions } from './store/module/app';
 
@@ -54,5 +54,5 @@ export async function unmount(props: any) {
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// or send to an analytics endpoint. Learn more in: https://bit.ly/CRA-vitals
 reportWebVitals();
