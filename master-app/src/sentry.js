1f1b3c478a9984fd63ef5494b2e0668d55d3471b
wayneliang(梁宇浩) <wayneliang@futunn.com>
2023-01-11 21:42:14 +0800

    save

--- master-app/src/sentry.js
+++ master-app/src/sentry.js
@@ -2,68 +2,67 @@ import Vue from 'vue'
 import * as Sentry from '@sentry/vue'
 import { BrowserTracing } from '@sentry/tracing'
 // import info from '../docker.json'
-import { makeFetchTransport } from '@sentry/browser'
+// import { makeFetchTransport } from '@sentry/browser'
 
+// const isProd = process.env.NODE_ENV === 'production'
 
-const isProd = process.env.NODE_ENV === 'production'
+// const dnsMap = {
+//   'master-app': { client: 'vue', dns: 'https://00be4200d6324e4b9ac81b465b120d81@o4504474273841152.ingest.sentry.io/4504478936006656' },
+//   'vue-app': { client: 'vue', dns: 'https://a3540506352d4f339a844c2d7b8280d7@o4504474273841152.ingest.sentry.io/4504474276986882' },
+//   'react-ts-app': { client: 'react', dns: 'https://84316ff2c0984ecfb1c20f93a4bb488d@o4504474273841152.ingest.sentry.io/4504474276986880' },
+//   'vue3-ts-app': { client: 'vue', dns: 'https://58e800f3bb5e49a3bf99515b8d51c5e3@o4504474273841152.ingest.sentry.io/4504478931877888' }
+// }
 
-const dnsMap = {
-  'master-app': { client: 'vue', dns: 'https://00be4200d6324e4b9ac81b465b120d81@o4504474273841152.ingest.sentry.io/4504478936006656' },
-  'vue-app': { client: 'vue', dns: 'https://a3540506352d4f339a844c2d7b8280d7@o4504474273841152.ingest.sentry.io/4504474276986882' },
-  'react-ts-app': { client: 'react', dns: 'https://84316ff2c0984ecfb1c20f93a4bb488d@o4504474273841152.ingest.sentry.io/4504474276986880' },
-  'vue3-ts-app': { client: 'vue', dns: 'https://58e800f3bb5e49a3bf99515b8d51c5e3@o4504474273841152.ingest.sentry.io/4504478931877888' }
-}
+// function sentryFilter(url, options) {
+//   console.log(url, options)
+//   let app
+//   console.log(options.body.includes('"type":"Error"'))
+//   if (options.body.includes('"type":"Error"')) {
+//     const [, filename] = options.body.match(/"filename":"([^"]*)"/)
+//     if (filename) {
+//       if (isProd) {
+//         if (filename.includes('app-react')) {
+//           app = 'react-ts-app'
+//         } else if (filename.includes('app-vue')) {
+//           app = 'vue-app'
+//         } else if (filename.includes('app-vue3')) {
+//           app = 'vue3-ts-app'
+//         } else {
+//           app = 'master-app'
+//         }
+//       } else {
+//         if (filename.includes('localhost:3001')) {
+//           app = 'react-ts-app'
+//         } else if (filename.includes('localhost:3002')) {
+//           app = 'vue-app'
+//         } else if (filename.includes('localhost:3004')) {
+//           app = 'vue3-ts-app'
+//         } else {
+//           app = 'master-app'
+//         }
+//       }
+//     }
+//     console.log('app', app)
+//     if (dnsMap[app]) {
+//       const { dns, client } = dnsMap[app]
+//       const { username, host, pathname } = new URL(dns)
+//       const newUrl = `https://${host}/api/${pathname.slice(1)}/store/?sentry_key=${username}&sentry_version=7&sentry_client=sentry.javascript.${client}/7.29.0`
+//       return [newUrl, options]
+//     } else {
+//       return [url, options]
+//     }
+//   } else {
+//     return [url, options]
+//   }
+// }
 
-function sentryFilter(url, options) {
-  console.log(url, options)
-  let app
-  console.log(options.body.includes('"type":"Error"'))
-  if (options.body.includes('"type":"Error"')) {
-    const [, filename] = options.body.match(/"filename":"([^"]*)"/)
-    if (filename) {
-      if (isProd) {
-        if (filename.includes('app-react')) {
-          app = 'react-ts-app'
-        } else if (filename.includes('app-vue')) {
-          app = 'vue-app'
-        } else if (filename.includes('app-vue3')) {
-          app = 'vue3-ts-app'
-        } else {
-          app = 'master-app'
-        }
-      } else {
-        if (filename.includes('localhost:3001')) {
-          app = 'react-ts-app'
-        } else if (filename.includes('localhost:3002')) {
-          app = 'vue-app'
-        } else if (filename.includes('localhost:3004')) {
-          app = 'vue3-ts-app'
-        } else {
-          app = 'master-app'
-        }
-      }
-    }
-    console.log('app', app)
-    if (dnsMap[app]) {
-      const { dns, client } = dnsMap[app]
-      const { username, host, pathname } = new URL(dns)
-      const newUrl = `https://${host}/api/${pathname.slice(1)}/store/?sentry_key=${username}&sentry_version=7&sentry_client=sentry.javascript.${client}/7.29.0`
-      return [newUrl, options]
-    } else {
-      return [url, options]
-    }
-  } else {
-    return [url, options]
-  }
-}
-
-const CustomeTransport = (options) => {
-  const fetchImpl = (url, options) => {
-    const [newUrl, newOptions] = sentryFilter(url, options)
-    return window.fetch(newUrl, newOptions)
-  }
-  return makeFetchTransport(options, fetchImpl)
-}
+// const CustomeTransport = (options) => {
+//   const fetchImpl = (url, options) => {
+//     const [newUrl, newOptions] = sentryFilter(url, options)
+//     return window.fetch(newUrl, newOptions)
+//   }
+//   return makeFetchTransport(options, fetchImpl)
+// }
 
 export function initSentry(router) {
   Sentry.init({
@@ -83,6 +82,10 @@ export function initSentry(router) {
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
     tracesSampleRate: 1.0,
-    transport: CustomeTransport
+    beforeSend(event, hint) {
+      console(event, hint)
+      return event
+    }
+    // transport: CustomeTransport
   })
 }
