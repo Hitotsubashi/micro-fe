df7615ecb9d41e5a136ecf68b2053c7f298f7af8
wayneliang(梁宇浩) <wayneliang@futunn.com>
2023-01-12 22:30:51 +0800

    master save

--- master-app/src/sentry.js
+++ master-app/src/sentry.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import * as Sentry from '@sentry/vue'
 import { BrowserTracing } from '@sentry/tracing'
-// import info from '../docker.json'
-// import { makeFetchTransport } from '@sentry/browser'
+import info from '../docker.json'
+import { makeFetchTransport } from '@sentry/browser'
 
-// const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production'
 
 // const dnsMap = {
 //   'master-app': { client: 'vue', dns: 'https://00be4200d6324e4b9ac81b465b120d81@o4504474273841152.ingest.sentry.io/4504478936006656' },
@@ -56,13 +56,63 @@ import { BrowserTracing } from '@sentry/tracing'
 //   }
 // }
 
-// const CustomeTransport = (options) => {
-//   const fetchImpl = (url, options) => {
-//     const [newUrl, newOptions] = sentryFilter(url, options)
-//     return window.fetch(newUrl, newOptions)
-//   }
-//   return makeFetchTransport(options, fetchImpl)
-// }
+const CustomeTransport = (options) => {
+  const fetchImpl = (url, options) => {
+    const [newUrl, newOptions] = sentryFilter(url, options)
+    return window.fetch(newUrl, newOptions)
+  }
+  return makeFetchTransport(options, fetchImpl)
+}
+
+const releaseMap = {
+  'master-app': `${info.name}@${info.version}`,
+  'vue-app': undefined,
+  'react-ts-app': 'react-ts-app@0.1.18',
+  'vue3-ts-app': undefined
+}
+
+function sentryFilter(url, options) {
+  console.log(url, options)
+  let app
+  console.log('includes:',options.body.includes('"type":"Error"'))
+  if (options.body.includes('"type":"Error"')) {
+    const [, filename] = options.body.match(/"filename":"([^"]*)"/)
+    if (filename) {
+      if (isProd) {
+        if (filename.includes('app-react')) {
+          app = 'react-ts-app'
+        } else if (filename.includes('app-vue')) {
+          app = 'vue-app'
+        } else if (filename.includes('app-vue3')) {
+          app = 'vue3-ts-app'
+        } else {
+          app = 'master-app'
+        }
+      } else {
+        if (filename.includes('localhost:3001')) {
+          app = 'react-ts-app'
+        } else if (filename.includes('localhost:3002')) {
+          app = 'vue-app'
+        } else if (filename.includes('localhost:3004')) {
+          app = 'vue3-ts-app'
+        } else {
+          app = 'master-app'
+        }
+      }
+    }
+    console.log('app', app)
+    if (releaseMap[app]) {
+      const release = releaseMap[app]
+      options.body = options.body.replace(/"release":"([^"]*)"/,`"release":"${release}"`)
+      return [url, options]
+    } else {
+      return [url, options]
+    }
+  } else {
+    return [url, options]
+  }
+}
+
 
 export function initSentry(router) {
   Sentry.init({
@@ -75,12 +125,13 @@ export function initSentry(router) {
         tracePropagationTargets: ['localhost', 'my-site-url.com', /^\//]
       })
     ],
-    // release: `${info.name}@${info.version}`,
+    release: `${info.name}@${info.version}`,
     environment: process.env.NODE_ENV,
     attachStacktrace: true,
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
     tracesSampleRate: 1.0,
+    transport: CustomeTransport,
   })
 }
