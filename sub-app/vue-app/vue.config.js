a5452c22b0cf2e3aa432920853070b6c7a0d49db
wayneliang(梁宇浩) <wayneliang@futunn.com>
2023-01-18 15:58:34 +0800

    save

--- sub-app/vue-app/vue.config.js
+++ sub-app/vue-app/vue.config.js
@@ -6,7 +6,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = defineConfig({
   parallel: false,
-  // transpileDependencies: true,
+  transpileDependencies: true,
   publicPath: isProd ? "/vue-app/" : undefined,
   devServer: {
     headers: {
@@ -19,9 +19,6 @@ module.exports = defineConfig({
   configureWebpack: {
     devtool: "source-map",
     output: {
-      filename: "js/[name]-[contenthash:8].js",
-      chunkFilename: "js/[name]-[contenthash:8].chunk.js",
-      assetModuleFilename: "css/[name].[hash][ext]",
       library: `${name}-[name]`,
       libraryTarget: "umd", // 把微应用打包成 umd 库格式
       chunkLoadingGlobal: `webpackJsonp_${name}`,
