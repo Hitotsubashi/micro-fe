475b8bf930a5b5c46313b213c64b13f8a8596a0d
wayneliang(梁宇浩) <wayneliang@futunn.com>
2023-01-28 17:41:45 +0800

    add error type

--- sub-app/react-ts-app/src/pages/index.tsx
+++ sub-app/react-ts-app/src/pages/index.tsx
@@ -5,6 +5,15 @@ import ThemeColor from '@/components/ThemeColor';
 import './index.css';
 
 function App() {
+  function throwPromiseError() {
+    return Promise.reject('vue-app promise error1');
+  }
+  function throwSetTimeoutError() {
+    setTimeout(() => {
+      throw new Error('vue-app settimeout error1');
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +28,8 @@ function App() {
         >
           error
         </button>
+        <button onClick={throwPromiseError}>PromiseError</button>
+        <button onClick={throwSetTimeoutError}>SetTimeoutError</button>
       </header>
     </div>
   );
